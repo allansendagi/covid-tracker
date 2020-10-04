a4783bef154fbe67b573d5cc78a8ae7aa877fb5d
allansendagi <sendagiallan87@gmail.com>
2020-10-04 12:03:18 +0400

    adding files

---
+++ build/precache-manifest.4894e9389dc6c9b3e7c1146277004016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5eb865edbbf5e7a70e91c7eac5180cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "4f5f7a6f9e8dcfc4d75b",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5f7a6f9e8dcfc4d75b",
+    "url": "/static/js/main.a5b08fd0.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
